e3a85c7638dbd9dc5121ab3143503891ebb67cdb
Johan Ribom <jribom@kth.se>
2016-12-25 20:17:48 +0100

    Removed the glfw directory, and added a triangle with some shaders to the main.cpp file.

---
+++ game/CMakeFiles/goblet_2.exe.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/johan/Git/Github/goblet_2_reawakening/game/src/common/loadShader.cpp" "/home/johan/Git/Github/goblet_2_reawakening/game/CMakeFiles/goblet_2.exe.dir/src/common/loadShader.cpp.o"
+  "/home/johan/Git/Github/goblet_2_reawakening/game/src/main.cpp" "/home/johan/Git/Github/goblet_2_reawakening/game/CMakeFiles/goblet_2.exe.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "src"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ game/CMakeFiles/goblet_2.exe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/goblet_2.exe.dir/src/main.cpp.o"
+  "CMakeFiles/goblet_2.exe.dir/src/common/loadShader.cpp.o"
+  "goblet_2.exe.pdb"
+  "goblet_2.exe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/goblet_2.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

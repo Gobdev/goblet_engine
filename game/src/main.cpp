152caaaf9286602f492e3cefcc9f490d0b0f25ca
Johan Ribom <jribom@kth.se>
2016-12-31 14:27:17 +0100

    Updated main.cpp to move the sprite of goblet king.

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,6 +1,7 @@
 // Include standard headers
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
 
 // Include GLEW
 #include <GL/glew.h>
@@ -17,6 +18,12 @@ using namespace glm;
 
 #include <common/shader.hpp>
 #include <common/texture.hpp>
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
 
 int main( void )
 {
@@ -36,7 +43,7 @@ int main( void )
 	// Open a window and create its OpenGL context
 	window = glfwCreateWindow( 1920, 1080, "Goblet 2: Reawakening", NULL, NULL);
 	if( window == NULL ){
-		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
+		fprintf( stderr, "Failed to open GLFW window.\n" );
 		glfwTerminate();
 		return -1;
 	}
@@ -49,8 +56,8 @@ int main( void )
 		return -1;
 	}
 
-	// Ensure we can capture the escape key being pressed below
-	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+	// Set key callback.
+	glfwSetKeyCallback(window, key_callback);
 
 	// Dark blue background
 	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
@@ -71,10 +78,10 @@ int main( void )
 	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
 
 	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-	glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
+	glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 10.0f);
 	// Camera matrix
 	glm::mat4 View       = glm::lookAt(
-								glm::vec3(4,3,3), // Camera is at (4,3,3), in World Space
+								glm::vec3(0,0,-4), // Camera is at (4,3,3), in World Space
 								glm::vec3(0,0,0), // and looks at the origin
 								glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
 						   );
@@ -90,86 +97,26 @@ int main( void )
 	// Get a handle for our "myTextureSampler" uniform
 	GLuint TextureID  = glGetUniformLocation(programID, "myTextureSampler");
 
-	// Our vertices. Tree consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
+	// Our vertices. Three consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
 	// A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
-	static const GLfloat g_vertex_buffer_data[] = { 
-		-2.0f,-2.0f,-2.0f,
-		-2.0f,-2.0f, 2.0f,
-		-2.0f, 2.0f, 2.0f,
-		 2.0f, 2.0f,-2.0f,
-		-2.0f,-2.0f,-2.0f,
-		-2.0f, 2.0f,-2.0f,
-		 2.0f,-2.0f, 2.0f,
-		-2.0f,-2.0f,-2.0f,
-		 2.0f,-2.0f,-2.0f,
-		 2.0f, 2.0f,-2.0f,
-		 2.0f,-2.0f,-2.0f,
-		-2.0f,-2.0f,-2.0f,
-		-2.0f,-2.0f,-2.0f,
-		-2.0f, 2.0f, 2.0f,
-		-2.0f, 2.0f,-2.0f,
-		 2.0f,-2.0f, 2.0f,
-		-2.0f,-2.0f, 2.0f,
-		-2.0f,-2.0f,-2.0f,
-		-2.0f, 2.0f, 2.0f,
-		-2.0f,-2.0f, 2.0f,
-		 2.0f,-2.0f, 2.0f,
-		 2.0f, 2.0f, 2.0f,
-		 2.0f,-2.0f,-2.0f,
-		 2.0f, 2.0f,-2.0f,
-		 2.0f,-2.0f,-2.0f,
-		 2.0f, 2.0f, 2.0f,
-		 2.0f,-2.0f, 2.0f,
-		 2.0f, 2.0f, 2.0f,
-		 2.0f, 2.0f,-2.0f,
-		-2.0f, 2.0f,-2.0f,
-		 2.0f, 2.0f, 2.0f,
-		-2.0f, 2.0f,-2.0f,
-		-2.0f, 2.0f, 2.0f,
-		 2.0f, 2.0f, 2.0f,
-		-2.0f, 2.0f, 2.0f,
-		 2.0f,-2.0f, 2.0f
+	GLfloat g_vertex_buffer_data[] = { 
+		-1.0f,-1.0f, 0.0f, 
+		-1.0f, 1.0f, 0.0f, 
+		 1.0f, 1.0f, 0.0f, 
+		-1.0f,-1.0f, 0.0f, 
+		 1.0f,-1.0f, 0.0f, 
+		 1.0f, 1.0f, 0.0f, 
 	};
 
-	// Two UV coordinatesfor each vertex. They were created withe Blender.
+	// Two UV coordinatesfor each vertex. They were created with Blender.
 	static const GLfloat g_uv_buffer_data[] = { 
-		0.000059f, 1.0f-0.000004f, 
-		0.000103f, 1.0f-0.336048f, 
-		0.335973f, 1.0f-0.335903f, 
-		1.000023f, 1.0f-0.000013f, 
-		0.667979f, 1.0f-0.335851f, 
-		0.999958f, 1.0f-0.336064f, 
-		0.667979f, 1.0f-0.335851f, 
-		0.336024f, 1.0f-0.671877f, 
-		0.667969f, 1.0f-0.671889f, 
-		1.000023f, 1.0f-0.000013f, 
-		0.668104f, 1.0f-0.000013f, 
-		0.667979f, 1.0f-0.335851f, 
-		0.000059f, 1.0f-0.000004f, 
-		0.335973f, 1.0f-0.335903f, 
-		0.336098f, 1.0f-0.000071f, 
-		0.667979f, 1.0f-0.335851f, 
-		0.335973f, 1.0f-0.335903f, 
-		0.336024f, 1.0f-0.671877f, 
-		1.000004f, 1.0f-0.671847f, 
-		0.999958f, 1.0f-0.336064f, 
-		0.667979f, 1.0f-0.335851f, 
-		0.668104f, 1.0f-0.000013f, 
-		0.335973f, 1.0f-0.335903f, 
-		0.667979f, 1.0f-0.335851f, 
-		0.335973f, 1.0f-0.335903f, 
-		0.668104f, 1.0f-0.000013f, 
-		0.336098f, 1.0f-0.000071f, 
-		0.000103f, 1.0f-0.336048f, 
-		0.000004f, 1.0f-0.671870f, 
-		0.336024f, 1.0f-0.671877f, 
-		0.000103f, 1.0f-0.336048f, 
-		0.336024f, 1.0f-0.671877f, 
-		0.335973f, 1.0f-0.335903f, 
-		0.667969f, 1.0f-0.671889f, 
-		1.000004f, 1.0f-0.671847f, 
-		0.667979f, 1.0f-0.335851f
-	};
+		0.0f, 0.0f, 
+		0.0f, 1.0f, 
+		1.0f, 1.0f, 
+		0.0f, 0.0f, 
+		1.0f, 0.0f, 
+		1.0f, 1.0f, 
+		};
 
 	GLuint vertexbuffer;
 	glGenBuffers(1, &vertexbuffer);
@@ -182,6 +129,18 @@ int main( void )
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_uv_buffer_data), g_uv_buffer_data, GL_STATIC_DRAW);
 
 	do{
+
+		for (int i = 0; i < 6; i++){
+			g_vertex_buffer_data[3*i] -= 0.01f;
+		}
+
+		glGenBuffers(1, &vertexbuffer);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+		//glm::mat4 ProjectionMatrix = Projection();
+		//glm::mat4 ViewMatrix = getViewMatrix();
+		//glm::mat4 ModelMatrix = glm::mat4(1.0);
+		glm::mat4 MVP = Projection * View * Model;
 
 		// Clear the screen
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -224,7 +183,7 @@ int main( void )
 		);
 
 		// Draw the triangle !
-		glDrawArrays(GL_TRIANGLES, 0, 12*3); // 12*3 indices starting at 0 -> 12 triangles
+		glDrawArrays(GL_TRIANGLES, 0, 2*3); // 12*3 indices starting at 0 -> 12 triangles
 
 		glDisableVertexAttribArray(0);
 		glDisableVertexAttribArray(1);
@@ -233,9 +192,9 @@ int main( void )
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 
-	} // Check if the ESC key was pressed or the window was closed
-	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-		   glfwWindowShouldClose(window) == 0 );
+
+	} // Loop while the window is open.
+	while( glfwWindowShouldClose(window) == 0 );
 
 	// Cleanup VBO and shader
 	glDeleteBuffers(1, &vertexbuffer);
